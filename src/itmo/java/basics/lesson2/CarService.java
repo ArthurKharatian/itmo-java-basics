533d944b549240ec731a6a3679619398f4b9dbae
Kharatian <kharatian_arthur@mail.ru>
2025-06-09 21:10:49 +0300

    lesson 2

---
+++ src/itmo/java/basics/lesson2/CarService.java
@@ -0,0 +1,10 @@
+package itmo.java.basics.lesson2;
+
+public class CarService {
+    public static long totalPrice(Car car, Integer servicePrice) {
+        if (servicePrice == null) {
+            return car.getPrice();
+        }
+        return car.getPrice() + servicePrice;
+    }
+}
